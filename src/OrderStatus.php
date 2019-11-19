b300314018f9ae2e3077d549fd718f7f975e966a
Karl Pierce <fishish@gmail.com>
2019-11-19 15:38:33 -0800

    Adding OrderStatus enum

---
+++ src/OrderStatus.php
@@ -0,0 +1,11 @@
+<?php
+namespace KevinEm\LimeLightCRM;
+
+class OrderStatus
+{
+    const APPROVED = 2;
+    const VOID_REFUNDED = 6;
+    const DECLINED = 7;
+    const SHIPPED = 8;
+    const PENDING = 11;
+}
