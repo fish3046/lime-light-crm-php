3a34de8b1c8d1faba8f9df6fbc66021c54d3edad
Karl Pierce <kpierce@ratespecial.com>
2022-05-06 11:28:32 -0700

    Return Response object from SubscriptionService calls

--- src/Services/SubscriptionService.php
+++ src/Services/SubscriptionService.php
@@ -3,6 +3,7 @@
 namespace KevinEm\LimeLightCRM\Services;
 
 use KevinEm\LimeLightCRM\v1\LimeLightCRM;
+use KevinEm\LimeLightCRM\v1\Response;
 
 /**
  * Helper functions for managing product subscriptions
@@ -21,13 +22,14 @@ class SubscriptionService
     /**
      * Will stop an order/product subscription.  This is permanent until restartSubscription() is passed.
      *
-     * @see https://developer-prod.limelightcrm.com/#1481d7e3-c9d6-4f30-b73f-48cc39d00a05
+     * @see https://developer-prod.sticky.io/#40e36799-fc92-45d6-bffc-5a9aba944f37
      * @param int $orderId
      * @param int $productId
+     * @return Response
      */
-    public function holdSubscription(int $orderId, int $productId): void
+    public function holdSubscription(int $orderId, int $productId): Response
     {
-        $this->v1Engine
+        return $this->v1Engine
             ->orders()
             ->subscriptionOrderUpdate([
                 'order_id'   => $orderId,
@@ -39,17 +41,18 @@ class SubscriptionService
     /**
      * Resume an order/product subscription that was placed on hold by holdSubscription()
      *
-     * @see https://developer-prod.limelightcrm.com/#1481d7e3-c9d6-4f30-b73f-48cc39d00a05
+     * @see https://developer-prod.sticky.io/#40e36799-fc92-45d6-bffc-5a9aba944f37
      * @param int  $orderId
      * @param int  $productId
      * @param bool $preserveOriginalRebillDate Only works if product_id is not considered an upsell.
      *                                         Otherwise will act like false was passed
+     * @return Response
      */
-    public function restartSubscription(int $orderId, int $productId, bool $preserveOriginalRebillDate = true)
+    public function restartSubscription(int $orderId, int $productId, bool $preserveOriginalRebillDate = true): Response
     {
         $status = $preserveOriginalRebillDate ? 'reset' : 'start';
 
-        $this->v1Engine
+        return $this->v1Engine
             ->orders()
             ->subscriptionOrderUpdate([
                 'order_id'   => $orderId,
@@ -62,15 +65,16 @@ class SubscriptionService
      * Changes next order price in the subscription to be the supplied value for one iteration.  If $makePermanent
      * is true, this will be permanent for all future bills on this subscription.
      *
-     * @see https://developer-prod.limelightcrm.com/#1481d7e3-c9d6-4f30-b73f-48cc39d00a05
+     * @see https://developer-prod.sticky.io/#40e36799-fc92-45d6-bffc-5a9aba944f37
      * @param int    $orderId
      * @param int    $productId
      * @param string $price
      * @param bool   $makePermanent
+     * @return Response
      */
-    public function changeSubscriptionPrice(int $orderId, int $productId, string $price, bool $makePermanent = true)
+    public function changeSubscriptionPrice(int $orderId, int $productId, string $price, bool $makePermanent = true): Response
     {
-        $this->v1Engine
+        return $this->v1Engine
             ->orders()
             ->subscriptionOrderUpdate([
                 'order_id'                     => $orderId,
@@ -82,14 +86,15 @@ class SubscriptionService
     }
 
     /**
-     * @see https://developer-prod.limelightcrm.com/#1481d7e3-c9d6-4f30-b73f-48cc39d00a05
+     * @see https://developer-prod.sticky.io/#40e36799-fc92-45d6-bffc-5a9aba944f37
      * @param int    $orderId
      * @param int    $productId
      * @param string $date MM/DD/YYYY format
+     * @return Response
      */
-    public function changeRecurringDate(int $orderId, int $productId, string $date)
+    public function changeRecurringDate(int $orderId, int $productId, string $date): Response
     {
-        $this->v1Engine
+        return $this->v1Engine
             ->orders()
             ->subscriptionOrderUpdate([
                 'order_id'           => $orderId,
